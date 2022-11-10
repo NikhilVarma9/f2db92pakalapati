bded75d99dc803632fa58cb27df200950a369738
NikhilVarma9 <112139146+NikhilVarma9@users.noreply.github.com>
2022-11-09 23:37:46 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var intrested = require("./models/intrested"); 
+
+require('dotenv').config(); 
+const connectionString =  
+process.env.MONGO_CON 
+mongoose = require('mongoose'); 
+mongoose.connect(connectionString,  
+{useNewUrlParser: true, 
+useUnifiedTopology: true}); 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var bikesRouter = require('./routes/bikes');
+var IntrestedRouter = require('./routes/intrested');
 var gridbuildRouter = require('./routes/gridbuild');
-var selectorRouter = require('./routes/selector');
-
+var selectordRouter = require('./routes/selector');
+var resourceRouter = require('./routes/resource');
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,10 +34,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/bikes', bikesRouter);
+app.use('/Intrested', IntrestedRouter);
 app.use('/gridbuild', gridbuildRouter);
-app.use('/selector', selectorRouter);
-
+app.use('/selector', selectordRouter);
+app.use('/resource', resourceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,5 +54,39 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+async function recreateDB(){ 
+  // Delete everything 
+  await intrested.deleteMany(); 
+ 
+  let instance1 = new 
+intrested({subjectName:"OOPS",  section:'A', 
+rollnumber:25}); 
+  instance1.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("First object saved") 
+  }); 
+
+  let instance2 = new 
+  intrested({subjectName:"Webapps",  section:'B', 
+  rollnumber:27}); 
+    instance2.save( function(err,doc) { 
+        if(err) return console.error(err); 
+        console.log("Second object saved") 
+    }); 
+
+    let instance3 = new 
+    intrested({subjectName:"ADB",  section:'C', 
+    rollnumber:15}); 
+      instance3.save( function(err,doc) { 
+          if(err) return console.error(err); 
+          console.log("Third object saved") 
+      }); 
+} 
+ 
+let reseed = true; 
+if (reseed) { recreateDB();} 
+ 
 
 module.exports = app;
