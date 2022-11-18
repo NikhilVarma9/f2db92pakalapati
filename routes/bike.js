28a24947f7d6526c81379d25b268098176f7f0e8
NikhilVarma9 <112139146+NikhilVarma9@users.noreply.github.com>
2022-11-18 15:10:05 -0600

    Update bike.js

--- routes/bike.js
+++ routes/bike.js
@@ -5,3 +5,16 @@ var router = express.Router();
 /* GET bikes */ 
 router.get('/', bike_controlers.bike_view_all_Page ); 
 module.exports = router; 
+
+/* GET detail bike page */ 
+router.get('/detail', bike_controlers.bike_view_one_Page); 
+
+/* GET create bike page */ 
+router.get('/create', bike_controlers.bike_create_Page); 
+
+/* GET create update page */ 
+router.get('/update', bike_controlers.bike_update_Page); 
+
+/* GET delete bike page */ 
+router.get('/delete', bike_controlers.bike_delete_Page); 
+ 
